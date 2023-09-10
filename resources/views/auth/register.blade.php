6b464a48b9a72ffad00fe278ab772e194fbc649b
maximfateev <maxim.f@rocketfirm.com>
2023-09-11 00:34:12 +0600

    initial

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,7 +58,16 @@
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Подтвердите пароль')" />
 
+            <x-text-input id="password_confirmation" class="block mt-1 w-full border-2"
+                          type="password"
+                          name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
         <div class="mt-4">
             <input type="checkbox" name="checkbox" value="true" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2" required />
             <label for="checkbox" class="ml-2 text-sm text-gray-900">Принимаю условия <a data-modal-target="staticModal" data-modal-toggle="staticModal" class="font-medium cursor-pointer">Соглашения при регистрации</a></label>
@@ -91,17 +100,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Подтвердите пароль')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full border-2"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
